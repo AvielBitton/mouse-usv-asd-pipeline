68592489923f2b5ed61e5b197e643041d9f1ba45
avielbitton <avielbitton@gmail.com>
2025-12-19 14:37:10 +0200

    refactor: replace custom Logger with Python logging and move setup_logger to utils

--- ASD_Tool/pipeline/utils.py
+++ ASD_Tool/pipeline/utils.py
@@ -1,14 +1,32 @@
-import sys
+import os
+import logging
 
 
-class Logger(object):
-    def __init__(self, filename):
-        self.terminal = sys.stdout
-        self.log = open(filename, "w")
-
-    def write(self, message):
-        self.terminal.write(message)   # print to screen
-        self.log.write(message)        # write to file
-
-    def flush(self):
-        pass
+def setup_logger(log_path="outputs/output.log"):
+    """Setup logging to both console and file."""
+    # Create parent directory if missing
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    
+    # Create logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    
+    # Remove existing handlers to avoid duplicates
+    logger.handlers.clear()
+    
+    # Create formatter
+    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+    
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    
+    # File handler
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    return logger
